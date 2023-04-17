4427c81bcc000cfb0b0c2a1e3450ed2337e05773
mingzhixian <mingzhixian123@gmail.com>
2023-04-18 00:27:27 +0800

    更新V6.0 修复大部分bug、对可预见崩溃做出处理，优化触控，实现悬浮导航球，优化流畅度，减少启动时间

--- scrcpy_android/app/src/main/java/top/saymzx/scrcpy_android/Page2.kt
+++ scrcpy_android/app/src/main/java/top/saymzx/scrcpy_android/Page2.kt
@@ -2,12 +2,16 @@ package top.saymzx.scrcpy_android
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class Page2 : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_page2)
+    // 全屏显示
+    window.decorView.systemUiVisibility =
+      (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
   }
 
   // 自动恢复界面
