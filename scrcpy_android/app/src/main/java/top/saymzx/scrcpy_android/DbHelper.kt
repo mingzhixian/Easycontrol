f943593eb51c3da21608adeca1588125d5feb8d7
mingzhixian <mingzhixian123@gmail.com>
2023-04-25 23:46:48 +0800

    更新V7.2 支持通知栏关闭，优化触控灵敏度，支持帧率和码率自定义，修复全屏偶尔失效的bug

--- scrcpy_android/app/src/main/java/top/saymzx/scrcpy_android/DbHelper.kt
+++ scrcpy_android/app/src/main/java/top/saymzx/scrcpy_android/DbHelper.kt
@@ -1,5 +1,6 @@
 package top.saymzx.scrcpy_android
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
@@ -16,11 +17,28 @@ class DbHelper(
           "\t ip text,\n" +
           "\t name text PRIMARY KEY,\n" +
           "\t videoCodec text,\n" +
+          "\t videoBit integer,\n" +
+          "\t fps integer,\n" +
           "\t resolution integer)"
     )
   }
 
-  override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
-    // 暂不需要
+  @SuppressLint("Range")
+  override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    if (oldVersion < 2) {
+      // 新建列
+      db!!.execSQL("alter table DevicesDb add column fps integer")
+      db.execSQL("alter table DevicesDb add column videoBit integer")
+      // 更新旧数据的默认值
+      val cursor =
+        db.query("DevicesDb", null, null, null, null, null, null)
+      if (cursor.moveToFirst()) {
+        do {
+          val name = cursor.getString(cursor.getColumnIndex("name"))
+          db.execSQL("update DevicesDb set fps='30',videoBit='12000000' where name=='$name'")
+        } while (cursor.moveToNext())
+      }
+      cursor.close()
+    }
   }
 }
