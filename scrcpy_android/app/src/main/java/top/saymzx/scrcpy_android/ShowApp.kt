c8befce49756249b1f60dac9bd4ae4ba503c0aff
mingzhixian <mingzhixian123@gmail.com>
2023-06-05 17:13:12 +0800

    更新V8.9 修复全屏控制导航栏未隐藏问题 修复介绍页面错位问题 修复码率设置崩溃问题

--- scrcpy_android/app/src/main/java/top/saymzx/scrcpy_android/ShowApp.kt
+++ scrcpy_android/app/src/main/java/top/saymzx/scrcpy_android/ShowApp.kt
@@ -3,7 +3,10 @@ package top.saymzx.scrcpy_android
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
 
@@ -12,6 +15,14 @@ class ShowApp : Activity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_show_app)
+    // 全屏显示
+    window.decorView.systemUiVisibility =
+      (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+    // 设置异形屏
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      window.attributes.layoutInDisplayCutoutMode =
+        WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+    }
     // 设置隐私政策链接
     findViewById<TextView>(R.id.show_app_privacy).setOnClickListener {
       try {
@@ -19,6 +30,17 @@ class ShowApp : Activity() {
         intent.addCategory(Intent.CATEGORY_BROWSABLE)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
         intent.data = Uri.parse("https://github.com/mingzhixian/scrcpy/blob/master/PRIVACY.md")
+        startActivity(intent)
+      } catch (_: Exception) {
+      }
+    }
+    // 设置使用说明链接
+    findViewById<TextView>(R.id.show_app_how_to_use).setOnClickListener {
+      try {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.addCategory(Intent.CATEGORY_BROWSABLE)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        intent.data = Uri.parse("https://github.com/mingzhixian/scrcpy/blob/master/HOW_TO_USE.md")
         startActivity(intent)
       } catch (_: Exception) {
       }
