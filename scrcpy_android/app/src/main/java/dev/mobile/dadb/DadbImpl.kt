2aef83545cb0ffc2e3d6cf0ad4ed4119df25ca43
mingzhixian <mingzhixian123@gmail.com>
2023-06-24 09:18:40 +0800

    更新V11.0 大： 更新scrcpy版本为2.1 更新最低版本为安卓7 小： 修复安卓7等低版本无法使用的问题 修复长按添加按钮进行强制清理时崩溃的问题

---
+++ scrcpy_android/app/src/main/java/dev/mobile/dadb/DadbImpl.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2021 mobile.dev inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package dev.mobile.dadb
+
+import org.jetbrains.annotations.TestOnly
+import java.net.Socket
+
+internal class DadbImpl(
+  private val host: String,
+  private val port: Int,
+  private val keyPair: AdbKeyPair? = null
+) : Dadb {
+
+  private var connection: Pair<AdbConnection, Socket>? = null
+
+  override fun open(destination: String) = connection().open(destination)
+
+  override fun supportsFeature(feature: String): Boolean {
+    return connection().supportsFeature(feature)
+  }
+
+  override fun close() {
+    connection?.first?.close()
+  }
+
+  override fun toString() = "$host:$port"
+
+  @TestOnly
+  fun closeConnection() {
+    connection?.second?.close()
+  }
+
+  @Synchronized
+  private fun connection(): AdbConnection {
+    var connection = connection
+    if (connection == null || connection.second.isClosed) {
+      connection = newConnection()
+      this.connection = connection
+    }
+    return connection.first
+  }
+
+  private fun newConnection(): Pair<AdbConnection, Socket> {
+    val socket = Socket(host, port)
+    val adbConnection = AdbConnection.connect(socket, keyPair)
+    return adbConnection to socket
+  }
+}
