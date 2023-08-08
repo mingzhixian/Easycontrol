f052625b8c19fdbfacf6fa0f4ea60e1fd754d715
mingzhixian <mingzhixian123@gmail.com>
2023-08-08 20:06:24 +0800

    保存

---
+++ scrcpy_android/app/build/generated/source/buildConfig/release/top/saymzx/scrcpy/android/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package top.saymzx.scrcpy.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "top.saymzx.scrcpy.android";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1300;
+  public static final String VERSION_NAME = "13.0.0";
+}
